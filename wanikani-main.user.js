6fd58bb217dd895683c37e815e0188656631dcf9
aktonkin <aktonkin@wonderlandnightmares.onmicrosoft.com>
2020-10-20 19:32:51 +1100

    update

--- wanikani-main.user.js
+++ wanikani-main.user.js
@@ -9,81 +9,15 @@
 // @grant        none
 // ==/UserScript==
 
+import wanikaniStyles from './wanikani-main-user.css';
+
 let reviewButton = '.lessons-and-reviews .lessons-and-reviews__reviews-button';
 let lessonButton = '.lessons-and-reviews .lessons-and-reviews__lessons-button';
-let reviewColour = '#00aaff';
-let lessonColour = '#ff00aa';
-let reviewColourDarker = '#0279b5';
-let lessonColourDarker = '#bd007e';
-let reviewColourLighter = '#80d4ff';
-let lessonColourLighter = '#fa7acf';
-let white = '#ffffff';
 
 var style = document.createElement('style');
 
-style.innerHTML =
-    reviewButton + '.has-reviews { ' +
-        'animation: reviewPulse 2s infinite; ' +
-    '} ' +
-
-    lessonButton + '.has-lessons { ' +
-        'animation: lessonPulse 2s infinite; ' +
-    '} ' +
-
-    '.custom-critical-items { ' +
-        'padding: 12px 24px; ' +
-        'margin-bottom: 30px; ' +
-        'background: #f4f4f4; ' +
-    '} ' +
-
-    '.custom-critical-items section { ' +
-        'margin-bottom: 0; ' +
-    '} ' +
-
-    '.custom-critical-items section .progress-entries { ' +
-        'grid-template-columns: none; ' +
-        'display: flex; ' +
-    '} ' +
-
-    '.custom-critical-items section .progress-entry { ' +
-        'height: 40px; ' +
-    '} ' +
-
-    '.custom-critical-items section .progress-entry.radical, ' +
-    '.custom-critical-items section .progress-entry.kanji { ' +
-        'width: 40px; ' +
-    '} ' +
-
-    '.custom-critical-items .vocabulary-icon { ' +
-        'font-size: 21px; ' +
-        'height: 40px; ' +
-    '} ' +
-
-    '@keyframes reviewPulse { ' +
-        '0% { ' +
-            'box-shadow: 0 0 0 0 ' + reviewColourDarker + '; ' +
-        '} ' +
-        '70% { ' +
-            'box-shadow: 0 0 0 7px ' + white + '; ' +
-        '} ' +
-        '100% { ' +
-            'box-shadow: 0 0 0 0 ' + reviewColourLighter + '; ' +
-        '} ' +
-    '} ' +
-
-    '@keyframes lessonPulse { ' +
-        '0% { ' +
-            'box-shadow: 0 0 0 0 ' + lessonColourDarker + '; ' +
-        '} ' +
-        '70% { ' +
-            'box-shadow: 0 0 0 7px ' + white + '; ' +
-        '} ' +
-        '100% { ' +
-            'box-shadow: 0 0 0 0 ' + lessonColourLighter + '; ' +
-        '} ' +
-    '}'
-;
-
+style.innerHTML = wanikaniStyles;
+style.type = 'text/css';
 style.className = 'custom-style';
 
 (function() {
