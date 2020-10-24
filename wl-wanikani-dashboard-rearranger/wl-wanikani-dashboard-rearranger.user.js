014185f7a0f441fb881b7fb87b80cb754186d76a
aktonkin <aktonkin@wonderlandnightmares.onmicrosoft.com>
2020-10-24 18:36:14 +1100

    update

--- wl-wanikani-dashboard-rearranger/wl-wanikani-dashboard-rearranger.user.js
+++ wl-wanikani-dashboard-rearranger/wl-wanikani-dashboard-rearranger.user.js
@@ -21,6 +21,7 @@
      *************************************************/
     console.log('Running ' + scriptNameSpace + ' functions.');
     addRearrangerStyles();
+    hideCompleteProgressItems();
     console.log('All ' + scriptNameSpace + ' functions have loaded.');
 
 
@@ -51,17 +52,12 @@
      *  Hides the completed progress items.
      *************************************************/
     function hideCompleteProgressItems() {
-        let progressitems = $('.progress-and-forecast .dashboard-progress .progress-entries .progress-entry__pips');
+        let progressEntry = $('.progress-and-forecast .dashboard-progress .progress-entries .progress-entry');
     
-        $.each(progressitems, function(index, progressItem){
-            //let progressitem = progressItem.children;
-            
-            //let hasClass = progressitem.hasClass('.bg-grey-300');
-            //console.log(hasClass);
-            //console.log(progressitem);
-            // if (progressitem.hasClass()) {
-            //     console.log(progressitem);
-            // }
+        $(progressEntry).each(function() {
+            if ($(this).find('.progress-entry__pips .bg-gray-300').length = 0) {
+                $(this).addClass('hidden');
+            }
         });
     }
 })();
