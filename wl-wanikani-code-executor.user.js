dbed55ccdc3a9677077acdb9738c4b7c88198a0f
aktonkin <aktonkin@wonderlandnightmares.onmicrosoft.com>
2020-11-21 12:44:22 +1100

    update

--- wl-wanikani-code-executor.user.js
+++ wl-wanikani-code-executor.user.js
@@ -179,7 +179,7 @@
     async function executeCriticalItemsCode(wkofData) {
         console.log('Running Critical Items functions.');
         await setCriticalItemsDebugMode(false);
-        let getCritItemsData = await getCriticalItemsData(wkofData);
+        let getCritItemsData = await getCriticalItemsData(wkofData.CritItemsData);
         await generateCriticalItemsTableHTML(getCritItemsData);
         console.log('All Critical Items functions have loaded.');
     };
--- wl-wanikani-critical-items/wl-wanikani-critical-items.user.js
+++ wl-wanikani-critical-items/wl-wanikani-critical-items.user.js
@@ -113,7 +113,7 @@ function getCriticalItemsData(items) {
 
 function generateCriticalItemsTableHTML(criticalItemsData) {
     let getCriticalItemsHTML = generateCriticalItemsHTML(criticalItemsData);
-    let headerMessage = (items.length == 0) 
+    let headerMessage = (criticalItemsData.length == 0) 
                         ? 'Sorry no items are critical right now.'
                         : 'You have critical items you suck at!';
 
