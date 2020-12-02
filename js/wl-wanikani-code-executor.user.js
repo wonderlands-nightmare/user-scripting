c519862dc6280f94f247b47bf0d6f95464e706e5
aktonkin <aktonkin@wonderlandnightmares.onmicrosoft.com>
2020-12-02 21:08:34 +1100

    update

--- js/wl-wanikani-code-executor.user.js
+++ js/wl-wanikani-code-executor.user.js
@@ -125,7 +125,7 @@
 
     function reviewAndLessonButtonPulseEffect() {
         // Shortcut buttons
-        addReviewAndLessonButtonPulseEffect('.navigation-shortcuts .navigation-shortcut--lessons > a', '.navigation-shortcuts .navigation-shortcut--lessons > a > span').text(), '/lesson/session', 'has-lessons');
-        addReviewAndLessonButtonPulseEffect('.navigation-shortcuts .navigation-shortcut--reviews > a', '.navigation-shortcuts .navigation-shortcut--reviews > a > span').text(), '/review/start', 'has-reviews');
+        addReviewAndLessonButtonPulseEffect($('.navigation-shortcuts .navigation-shortcut--lessons > a', '.navigation-shortcuts .navigation-shortcut--lessons > a > span').text(), '/lesson/session', 'has-lessons');
+        addReviewAndLessonButtonPulseEffect($('.navigation-shortcuts .navigation-shortcut--reviews > a', '.navigation-shortcuts .navigation-shortcut--reviews > a > span').text(), '/review/start', 'has-reviews');
     };
 })();
