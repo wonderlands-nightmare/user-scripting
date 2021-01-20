a19196a79ff714f50d1be30438b9dc0a58568580
aktonkin <alice.k.tonkin@gmail.com>
2021-01-20 20:29:15 +1100

    updated

--- js/wl-wanikani-code-executor.user.js
+++ js/wl-wanikani-code-executor.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         WaniKani Custom Dashboard
 // @namespace    https://github.com/wonderlands-nightmare
-// @version      0.8
+// @version      1.0
 // @description  A collection of custom scripts for editing the wanikani experience.
 // @author       Wonderland-Nightmares
 // @match        https://www.wanikani.com/*
