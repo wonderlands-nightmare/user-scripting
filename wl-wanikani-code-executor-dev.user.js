3fb9d2e102a7ae9c2dc45deccc5bd70939d64696
aktonkin <alice.k.tonkin@gmail.com>
2021-02-27 17:34:27 +1100

    Add WKOF settings checker

--- wl-wanikani-code-executor-dev.user.js
+++ wl-wanikani-code-executor-dev.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         WaniKani Custom Dashboard - DEV
 // @namespace    https://github.com/wonderlands-nightmare
-// @version      1.4.2
+// @version      1.4.3
 // @description  A collection of custom scripts for editing the wanikani experience.
 // @author       Wonderland-Nightmares
 // @include      /^https://(www|preview).wanikani.com/(dashboard)?$/
@@ -111,6 +111,8 @@
     wkof.ready(wkofSettingsModules)
         .then(loadWkofMenu)
         .then(loadWkofSettings);
+
+    wkofSettingsChecker();
     
     if (window.location.href.match(dashboardUrlRegEx)) {
         console.log('running on ' + window.location.href);
