631884ee47956ef70399524434d0074e75e52e8f
aktonkin <alice.k.tonkin@gmail.com>
2021-02-27 17:13:06 +1100

    Update skip function for session home button

--- components/additional/wl-wanikani-additional.user.js
+++ components/additional/wl-wanikani-additional.user.js
@@ -3,6 +3,13 @@
  *************************************************/
 function skipReviewLessonSummary() {
     if (wkof.settings[scriptId].skip_session_summary) {
+        skipAfterLessonSession();
+        skipSummaryOnHomeButtonClick();
+    }
+};
+
+function skipAfterSession() {
+    if ($('#screen-lesson-done .btn-set').length > 0) {
         console.log('adding skip button');
         let skipLessonSummaryButton = `
             <li id="lesson-skip-to-homepage">
@@ -14,4 +21,11 @@ function skipReviewLessonSummary() {
 
         $('#screen-lesson-done .btn-set').append(skipLessonSummaryButton);
     }
-}
+};
+
+function skipSummaryOnHomeButtonClick() {
+    let sessionHomeButton = $('#question #summary-button > a');
+    if (sessionHomeButton.length > 0) {
+        sessionHomeButton.attr('href', '/dashboard');
+    }
+};
