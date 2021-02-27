ed767c5abcf5feffd153bf10051c06205f7ee119
aktonkin <alice.k.tonkin@gmail.com>
2021-02-27 18:53:48 +1100

    Added removal on setting change in session

--- components/additional/wl-wanikani-additional.user.js
+++ components/additional/wl-wanikani-additional.user.js
@@ -2,37 +2,58 @@
  *  ANCHOR Skip review/lesson summary after session
  *************************************************/
 function skipReviewLessonSummary() {
-    if (wkof.settings[scriptId].skip_session_summary) {
-        skipAfterLessonSession();
-        skipAfterReviewSession();
-        skipSummaryOnHomeButtonClick();
+    const skipSetting = wkof.settings[scriptId].skip_session_summary;
+    skipAfterLessonSession(skipSetting);
+    skipAfterReviewSession(skipSetting);
+    skipSummaryOnHomeButtonClick(skipSetting);
+};
+
+function skipAfterLessonSession(skip) {
+    let lessonButtonSet = $('#screen-lesson-done .btn-set');
+    if (lessonButtonSet.length > 0) {
+        if (lessonButtonSet.find('#lesson-skip-to-homepage') > 0) {
+            lessonButtonSet.find('#lesson-skip-to-homepage').remove();
+        }
+
+        if (skip) {
+            let skipLessonSummaryButton = `
+                <li id="lesson-skip-to-homepage">
+                    <a href="https://www.wanikani.com/dashboard">
+                        <i class="icon-arrow-left dominant"></i>
+                        Go to homepage
+                </li>
+            `;
+
+            lessonButtonSet.append(skipLessonSummaryButton);
+        }
     }
 };
 
-function skipAfterLessonSession() {
-    if ($('#screen-lesson-done .btn-set').length > 0) {
-        let skipLessonSummaryButton = `
-            <li id="lesson-skip-to-homepage">
-                <a href="https://www.wanikani.com/dashboard">
-                    <i class="icon-arrow-left dominant"></i>
-                    Go to homepage
-            </li>
-        `;
-
-        $('#screen-lesson-done .btn-set').append(skipLessonSummaryButton);
-    }
-};
-
-function skipAfterReviewSession() {
+function skipAfterReviewSession(skip) {
     let reviewInputButton = $('#reviews #question #answer-form fieldset > button');
     if (reviewInputButton.length > 0) {
-        reviewInputButton.attr('href', '/dashboard');
+        if (skip) {
+            reviewInputButton.attr('href', '/dashboard');
+        }
+        else {
+            reviewInputButton.removeAttr('href');
+        }
     }
 };
 
-function skipSummaryOnHomeButtonClick() {
+function skipSummaryOnHomeButtonClick(skip) {
     let sessionHomeButton = $('#question #summary-button > a');
     if (sessionHomeButton.length > 0) {
-        sessionHomeButton.attr('href', '/dashboard');
+        if (skip) {
+            sessionHomeButton.attr('href', '/dashboard');
+        }
+        else {
+            if (window.location.href.includes('review')) {
+                sessionHomeButton.attr('href', '/review');
+            }
+            if (window.location.href.includes('lesson')) {
+                sessionHomeButton.attr('href', '/lesson');
+            }
+        }
     }
 };
