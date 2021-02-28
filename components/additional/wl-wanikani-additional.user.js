e5a96ded13f82acf6fe5c94b7a02d64991032df5
aktonkin <alice.k.tonkin@gmail.com>
2021-02-28 14:38:34 +1100

    Adjustment for skip summary feature based on usage

--- components/additional/wl-wanikani-additional.user.js
+++ components/additional/wl-wanikani-additional.user.js
@@ -12,22 +12,42 @@
 
 // NOTE Add button to use after lesson session
 function skipAfterLessonSession(skip) {
-    let lessonButtonSet = $('#screen-lesson-done .btn-set');
-    if (lessonButtonSet.length > 0) {
-        if (lessonButtonSet.find('#lesson-skip-to-homepage') > 0) {
-            lessonButtonSet.find('#lesson-skip-to-homepage').remove();
-        }
-
+    let lessonReadyButton = $('#screen-lesson-ready .btn-set #lesson-ready-end');
+    let lessonReadyButtonOriginalContent = `
+        <i class="icon-arrow-left"></i>
+        I'm done for now
+    `;
+    let lessonDoneEndButton = $('#screen-lesson-done .btn-set #lesson-ready-end');
+    let lessonDoneButtonOriginalContent = `
+        <i class="icon-arrow-left dominant"></i>
+        View summary
+    `
+    let newButtonContent = `
+        <a class="lesson-skip-to-homepage" href="https://www.wanikani.com/dashboard">
+            <i class="icon-arrow-left dominant"></i>
+            Go to homepage
+        </a>
+    `;
+    
+    if (lessonReadyButton.length > 0) {
         if (skip) {
-            let skipLessonSummaryButton = `
-                <li id="lesson-skip-to-homepage">
-                    <a href="https://www.wanikani.com/dashboard">
-                        <i class="icon-arrow-left dominant"></i>
-                        Go to homepage
-                </li>
-            `;
+            lessonReadyButton.text(newButtonContent);
+        }
+        else {
+            if (lessonReadyButton.find('.lesson-skip-to-homepage').length > 0) {
+                lessonReadyButton.text(lessonReadyButtonOriginalContent);
+            }
+        }
+    }
 
-            lessonButtonSet.append(skipLessonSummaryButton);
+    if (lessonDoneEndButton.length > 0) {
+        if (skip) {
+            lessonDoneEndButton.text(newButtonContent);
+        }
+        else {
+            if (lessonDoneEndButton.find('.lesson-skip-to-homepage').length > 0) {
+                lessonDoneEndButton.text(lessonDoneButtonOriginalContent);
+            }
         }
     }
 };
@@ -37,9 +57,11 @@ function skipAfterLessonSession(skip) {
 function skipAfterReviewSession(skip) {
     if (skip) {
         $('#reviews #question #answer-form button').on('click.skipToHomepage', function() {
-            if ($('#reviews #question #stats #available-count').text() == 0) {
-                window.location = 'https://www.wanikani.com';
-            }
+            setTimeout(function() {
+                if ($('#reviews #question #stats #available-count').text() == 0) {
+                    window.location = 'https://www.wanikani.com';
+                }
+            }, 500);
         });
     }
     else {
@@ -50,18 +72,29 @@ function skipAfterReviewSession(skip) {
 
 // NOTE Change HREF on both session sets
 function skipSummaryOnHomeButtonClick(skip) {
-    let sessionHomeButton = $('#question #summary-button > a');
-    if (sessionHomeButton.length > 0) {
-        if (skip) {
-            sessionHomeButton.attr('href', '/dashboard');
+    let reviewSessionHomeButton = $('#question #summary-button > a');
+    let lessonSessionHomeButton = $('#header-buttons a#summary-button');
+    let newHref = '';
+    let newTitle = '';
+    
+    if (skip) {
+        newHref = '/dashboard';
+        newTitle = 'Go to homepage';
+    }
+    else {
+        newTitle = 'Go to summary page';
+        if (window.location.href.includes('review')) {
+            newHref = '/review';
         }
-        else {
-            if (window.location.href.includes('review')) {
-                sessionHomeButton.attr('href', '/review');
-            }
-            if (window.location.href.includes('lesson')) {
-                sessionHomeButton.attr('href', '/lesson');
-            }
+        if (window.location.href.includes('lesson')) {
+            newHref = '/lesson';
         }
+    }
+    
+    if (reviewSessionHomeButton.length > 0) {
+        reviewSessionHomeButton.attr('href', newHref).attr('title', newTitle);
+    }
+    if (lessonSessionHomeButton.length > 0) {
+        lessonSessionHomeButton.attr('href', newHref).attr('title', newTitle);
     }
 };
