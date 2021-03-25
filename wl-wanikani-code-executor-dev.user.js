fffdd5e166b81573f1c871567374f4f970bd7241
aktonkin <alice.k.tonkin@gmail.com>
2021-03-25 20:21:44 +1100

    Remove old reference

--- wl-wanikani-code-executor-dev.user.js
+++ wl-wanikani-code-executor-dev.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         WaniKani Custom Dashboard - DEV
 // @namespace    https://github.com/wonderlands-nightmare
-// @version      1.4.5.7
+// @version      1.4.5.8
 // @description  A collection of custom scripts for editing the wanikani experience.
 // @author       Wonderland-Nightmares
 // @include      /^https://(www|preview).wanikani.com/(dashboard)?$/
@@ -175,8 +175,7 @@
             if (resourceName == 'customTheme') {
                 customThemeCss = {
                     1: GM_getResourceText(dashboardResources[resourceName][1].css),
-                    2: GM_getResourceText(dashboardResources[resourceName][2].css),
-                    3: GM_getResourceText(dashboardResources[resourceName][3].css)
+                    2: GM_getResourceText(dashboardResources[resourceName][2].css)
                 }
             }
             else if (resourceName == 'customCompatabilityTheme') {
